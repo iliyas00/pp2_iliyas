ee2d143a3125a7e4590f4c9b29c2da3c95fb4c60
iliyas00 <zheksenov31@gmail.com>
2025-02-01 23:10:55 +0600

    add task2

---
+++ Lab_1/task2.py
@@ -0,0 +1,19 @@
+# syntax 
+# #example
+# if 5 > 2:
+#     print("Five is greater than two!")
+
+#syntax error
+# if 5 > 2:
+# print("Five is greater tha two!")
+
+# #example
+# if 5 > 2:
+#     print("Five is greater than two!")
+# if 5 > 2:
+#         print("Five is greater than two!")
+
+# #syntax error
+# if 5 > 2:
+#      print("Five is greater than two!")
+#         print("Five is greater than two!")
